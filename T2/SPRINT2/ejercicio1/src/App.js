11a0a59a5eb1326aca75cb5124ccc0d65c5bccc0
fercut <ferminycia@gmail.com>
2024-02-06 09:13:50 +0100

    ejercicio4

--- T2/SPRINT2/ejercicio1/src/App.js
+++ T2/SPRINT2/ejercicio1/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import FormularioRegistro from './components/FormularioRegistro';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Formulario de registro</h1>
+      <FormularioRegistro/>
     </div>
   );
 }
